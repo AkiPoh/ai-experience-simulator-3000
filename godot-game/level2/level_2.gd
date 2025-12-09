7dad5e342877966adc832e9f45dba9a479de56c7
AkiPoh <59505647+AkiPoh@users.noreply.github.com>
2025-12-09 20:28:10 +0200

    Add level2 to menu and duplicte level1 to be level2 G&A

---
+++ godot-game/level2/level_2.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+
+var coins_collected := 0
+var total_coins := 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	total_coins = $Coins.get_child_count()
+	for coin in $Coins.get_children():
+		coin.body_entered.connect(_on_coin_collected.bind(coin))
+	$CoinCounter.text = str(coins_collected) + "/" + str(total_coins)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_coin_collected(body: Node, coin: Node) -> void:
+	if body == $Vacuum:
+		coins_collected += 1
+		coin.queue_free()
+		$CoinCounter.text = str(coins_collected) + "/" + str(total_coins)
+		if coins_collected >= total_coins:
+			_on_level_complete()
+
+
+func _on_level_complete() -> void:
+	$WinOverlay.visible = true
+	await get_tree().create_timer(5).timeout
+	get_tree().change_scene_to_file("res://menu/menu.tscn")
--- godot-game/menu/menu.gd
+++ godot-game/menu/menu.gd
@@ -4,6 +4,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Level1Button.pressed.connect(_on_level1_pressed)
+	$Level2Button.pressed.connect(_on_level2_pressed)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,3 +14,7 @@ func _process(delta: float) -> void:
 
 func _on_level1_pressed() -> void:
 	get_tree().change_scene_to_file("res://level1/level_1.tscn")
+
+
+func _on_level2_pressed() -> void:
+	get_tree().change_scene_to_file("res://level2/level_2.tscn")
